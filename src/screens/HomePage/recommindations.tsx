b71fe68beee3849d9671d158a1fa2c1c0a94f8dc
Mirzo/미조 <mirzo@mijos-MacBook-Pro.local>
2023-11-15 15:08:52 +0900

    feat: develop homepage componenet

---
+++ src/screens/HomePage/recommindations.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+export function Recommindations() {
+  return (
+    <div className="top_article_frame">
+      <Container>Recommindations</Container>
+    </div>
+  );
+}
