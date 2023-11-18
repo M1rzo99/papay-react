4b2d5f18545558c9558663617fecf2eadee6bd11
Mirzo/미조 <mirzo@mijos-MacBook-Pro.local>
2023-11-18 20:11:07 +0900

    feat: homepage ads section publishing

--- src/screens/HomePage/bestDishes.tsx
+++ src/screens/HomePage/bestDishes.tsx
@@ -1,10 +1,51 @@
-import { Container } from "@mui/material";
+import { MonetizationOn } from "@mui/icons-material";
+import { Box } from "@mui/joy";
+import { Container, Stack } from "@mui/material";
+import { url } from "inspector";
 import React from "react";
 
 export function BestDishes() {
   return (
-    <div className="top_restaurant_frame">
-      <Container>BestDishes</Container>
+    <div className="best_dishes_frame">
+      <Container>
+        <Stack flexDirection={"column"} alignItems={"center"}>
+          <Box className="category_title">Trentdagi Ovqatlar</Box>
+          <Stack sx={{ mt: "43px" }} flexDirection={"row"}>
+            {[1, 2, 3, 4].map(() => {
+              return (
+                <Box className="dish_box">
+                  <Stack
+                    className="dish_img"
+                    sx={{
+                      backgroundImage: `url(
+                        https://www.cookerru.com/wp-content/uploads/2021/07/ganjang-guksu-ingredients-web-scaled.jpg
+                  )`,
+                    }}
+                  >
+                    <div className={"dish_sale"}>normal size</div>
+                    <div className={"view_btn"}>
+                      Batafsil ko'rish{" "}
+                      <img
+                        src={"/icons/arrow_right.svg"}
+                        style={{ marginLeft: "9px" }}
+                        alt=""
+                      />
+                    </div>
+                  </Stack>
+
+                  <Stack className={"dish_desc"}>
+                    <span className={"dish_title_text"}>Natural Oil</span>
+                    <span className={"dish_desc_text"}>
+                      <MonetizationOn />
+                      11
+                    </span>
+                  </Stack>
+                </Box>
+              );
+            })}
+          </Stack>
+        </Stack>
+      </Container>
     </div>
   );
 }
