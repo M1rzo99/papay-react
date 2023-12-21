5d1318b92f4fa0471898f9d9c831c43bbabdc646
lucas <accaunoff99@gmail.com>
2023-12-21 13:55:52 +0900

    fix : usage of slice and selector on homaPage screen component

--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -9,14 +9,46 @@ import { Events } from "./events";
 import { Recommindations } from "./recommindations";
 import "../../../css/home.css";
 
-export function HomePage() {
-  // useEffect(() => {
-  //   console.log("componenetDidMount = Malumotlarni olish");
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import {
+  setTopRestaurants,
+  bestRestaurants,
+} from "../../screens/HomePage/slice";
+import { retrieveTopRestaurants } from "../../screens/HomePage/selector";
+import { Restaurant } from "../../../types/user";
 
-  //   return () => {
-  //     console.log("componenetWillMount process");
-  //   };
-  // }, []); // componenet birinchi mount bolganda ishga tushadi
+//  REDUX SLICE
+const actionDispatch = (dispach: Dispatch) => ({
+  setTopRestaurants: (data: Restaurant[]) => dispach(setTopRestaurants(data)),
+});
+
+//REDUX SELECTOR
+const topRestaurantRetriver = createSelector(
+  retrieveTopRestaurants,
+  (topRestaurants) => ({
+    topRestaurants,
+  })
+);
+
+export function HomePage() {
+  // INITIALIZATIONS
+  const { setTopRestaurants } = actionDispatch(useDispatch());
+  const { topRestaurants } = useSelector(topRestaurantRetriver);
+
+  console.log("toprestaurant:::", topRestaurants);
+
+  // selector: store => data -- storagedan olgan ma'lumotni o'qiydi
+
+  useEffect(() => {
+    // backendan data request => data
+
+    setTopRestaurants([]);
+
+    // backend olingan datani storage ga yozadi: slice: data=>store
+  }, []);
 
   return (
     <div className="homepage">
